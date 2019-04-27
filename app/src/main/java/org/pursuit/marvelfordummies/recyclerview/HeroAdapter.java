87f445ac020f81aa3e4562d550b9b66b34411a98
garganjohn <garganjohn@gmail.com>
2019-04-27 13:41:16 -0400

    placeholder lists/objects placed in adapter and viewholder no logic to initialize recyclerview

--- app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroAdapter.java
+++ app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroAdapter.java
@@ -1,4 +1,33 @@
 package org.pursuit.marvelfordummies.recyclerview;
 
-public class HeroAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.pursuit.marvelfordummies.R;
+
+import java.util.List;
+
+public class HeroAdapter extends RecyclerView.Adapter<HeroViewHolder> {
+    //TODO change genericList to list of objects from endpoint
+    List<Object> genericList;
+
+    @NonNull
+    @Override
+    public HeroViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View childView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.hero_itemview, viewGroup, false);
+        return new HeroViewHolder(childView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HeroViewHolder heroViewHolder, int i) {
+        heroViewHolder.onBind(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return genericList.size();
+    }
 }
--- app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroViewHolder.java
+++ app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroViewHolder.java
@@ -1,4 +1,23 @@
 package org.pursuit.marvelfordummies.recyclerview;
 
-public class HeroViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.pursuit.marvelfordummies.R;
+
+class HeroViewHolder extends RecyclerView.ViewHolder {
+    //TODO create relevant views
+    private TextView genericTextView;
+
+    HeroViewHolder(@NonNull View itemView) {
+        super(itemView);
+        genericTextView = itemView.findViewById(R.id.hero_name_itemview);
+    }
+
+    //TODO Pass object/primitve onBind needs
+    void onBind(int i) {
+        genericTextView.setText(String.valueOf(i));
+    }
 }
