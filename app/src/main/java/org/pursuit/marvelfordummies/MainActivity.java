c2b5f2413c10cc65fb94a6e644bb5f4f475b33d5
garganjohn <garganjohn@gmail.com>
2019-04-27 14:39:04 -0400

    recyclerview now initializes with dummy data

--- app/src/main/java/org/pursuit/marvelfordummies/MainActivity.java
+++ app/src/main/java/org/pursuit/marvelfordummies/MainActivity.java
@@ -2,15 +2,35 @@ package org.pursuit.marvelfordummies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.pursuit.marvelfordummies.recyclerview.HeroAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity.DATA";
+
+    private List<HeroSummary> heroSummaryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        heroSummaryList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            heroSummaryList.add(new DummyHero());
+        }
+
+        initHeroRecyclerView();
 
     }
 
+    void initHeroRecyclerView(){
+        RecyclerView heroRecyclerView = findViewById(R.id.hero_recyclerview);
+        heroRecyclerView.setAdapter(new HeroAdapter(heroSummaryList));
+        heroRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
--- app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroAdapter.java
+++ app/src/main/java/org/pursuit/marvelfordummies/recyclerview/HeroAdapter.java
@@ -6,23 +6,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.pursuit.marvelfordummies.DummyHero;
+import org.pursuit.marvelfordummies.HeroSummary;
 import org.pursuit.marvelfordummies.R;
 
 import java.util.List;
 
 public class HeroAdapter extends RecyclerView.Adapter<HeroViewHolder> {
     //TODO change genericList to list of objects from endpoint
-    List<Object> genericList;
+    private List<HeroSummary> heroSummaryList;
 
-    public HeroAdapter(List<Object> genericList) {
-        this.genericList = genericList;
+    public HeroAdapter(List<HeroSummary> genericList) {
+        this.heroSummaryList = genericList;
     }
 
     @NonNull
     @Override
     public HeroViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View childView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.hero_itemview, viewGroup, false);
-        return new HeroViewHolder(childView);
+        return new HeroViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.hero_itemview, viewGroup, false));
     }
 
     @Override
@@ -32,6 +33,6 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroViewHolder> {
 
     @Override
     public int getItemCount() {
-        return genericList.size();
+        return heroSummaryList.size();
     }
 }
