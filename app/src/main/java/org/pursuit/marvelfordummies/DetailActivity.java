21dea7654328a4d0bf0870db4a85c2437eb64a22
garganjohn <garganjohn@gmail.com>
2019-04-28 11:42:37 -0400

    link button now opens heros wiki page

--- app/src/main/java/org/pursuit/marvelfordummies/DetailActivity.java
+++ app/src/main/java/org/pursuit/marvelfordummies/DetailActivity.java
@@ -2,12 +2,15 @@ package org.pursuit.marvelfordummies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.pursuit.marvelfordummies.data.model.Hero;
 
@@ -16,12 +19,13 @@ public class DetailActivity extends AppCompatActivity {
     private static String heroDescription;
     private static String heroThumbnail;
 
-    public static final String HERO_NAME = "HERO NAME";
-    public static final String HERO_DESCRIPTION = "HERO DESC";
-    public static final String HERO_THUMBNAIL = "HERO THUMBNAIL";
+    private static final String HERO_PATH = "https://marvelcinematicuniverse.fandom.com/wiki/";
+    private static final String HERO_NAME = "HERO NAME";
+    private static final String HERO_DESCRIPTION = "HERO DESC";
+    private static final String HERO_THUMBNAIL = "HERO THUMBNAIL";
 
-    private static ImageView heroImage;
-    private static TextView bioTextView;
+    private ImageView heroImage;
+    private TextView bioTextView;
     private Button linkBtn;
 
     @Override
@@ -31,15 +35,12 @@ public class DetailActivity extends AppCompatActivity {
         initViews();
 
         getHeroIntent();
-        //Picasso.get().load(hero.getImage()).into(heroImage);
-        //bioTextView.setText(hero.description);
+        Picasso.get().load(heroThumbnail).into(heroImage);
+        bioTextView.setText(heroDescription);
 
-        linkBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Link to mcu
-
-            }
+        linkBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(HERO_PATH + heroName));
+            startActivity(intent);
         });
     }
 
