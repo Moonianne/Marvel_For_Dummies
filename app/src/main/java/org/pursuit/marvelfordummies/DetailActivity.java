09435efb8e1571aa7d5ad037b216365a457ef129
garganjohn <garganjohn@gmail.com>
2019-04-28 11:28:27 -0400

    rebase for master

--- app/src/main/java/org/pursuit/marvelfordummies/DetailActivity.java
+++ app/src/main/java/org/pursuit/marvelfordummies/DetailActivity.java
@@ -38,6 +38,7 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Link to mcu
+
             }
         });
     }
